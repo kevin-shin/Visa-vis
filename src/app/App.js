61d18ef2e30f15e66c17681ae00c0bfff8758148
Juan David Garrido <juandagarrido@hotmail.com>
2019-12-02 13:30:54 -0600

    Basic structure

---
+++ src/app/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="main">
+      <div className="timeline">
+
+      </div>
+      <div className="tasks-list">
+        <ul>
+          <li>
+            task 1
+          </li>
+        </ul>
+      </div>
+      <div className="task-description">
+        Task Description
+      </div>
+    </div>
+  );
+};
+
+const TasksList = () => {
+
+};
+
+
+export default App;
---
+++ src/common/mock.js
@@ -0,0 +1,14 @@
+export const MOCK_TASKS = [
+  {
+    date: "10/10/10",
+    checked: false,
+    title: "Some Title #1",
+    description: "Some description #1"
+  },
+  {
+    date: "12/10/10",
+    checked: false,
+    title: "Some Title #2",
+    description: "Some description #2"
+  }
+];
