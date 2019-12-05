84cd85a539254c303f05d320d0820a4359e51bc7
razakhalid <rkhalid@macalester.edu>
2019-12-05 16:26:49 -0600

    updated information

--- src/common/mock.js
+++ src/common/mock.js
@@ -1,9 +1,9 @@
 export const MOCK_TASKS = [
   {
     id: 0,
-    date: "1999-01-01",
+    date: "2020-03-15",
     checked: false,
-    title: "Copy of Passport Photo",
+    title: "Copy of passport photo page",
     description: "Two passport-style photos taken within 30 days at FedEx, Walgreens, CVS, etc. Write lightly in pencil or felt pen your name and I-94 number) on back of photos (or card envelope if available).",
     notes: "",
     subTasks: [
@@ -28,15 +28,15 @@ export const MOCK_TASKS = [
   },
   {
     id: 1,
-    date: "1999-01-01",
+    date: "2020-04-01",
     checked: false,
-    title: "Some Title #2",
-    description: "Some description #2",
-    notes: "",
+    title: "OPT application supplement",
+    description: "You must complete this form BEFORE your appointment with an ISP staff member to review your OPT or OPT STEM (after you complete 12-month OPT) application and to issue a new I-20 to include with your application.",
+    notes: "Your OPT or OPT STEM application must be received by USCIS within 30 days of I-20 issuance; the consequence for not doing so is rejection of your OPT or OPT STEM application.",
     subTasks: [
       {
         id: 0,
-        title: "Subtask #1",
+        title: "Fill out the application supplement",
         checked: false
       }
     ],
@@ -44,26 +44,26 @@ export const MOCK_TASKS = [
       {
         id: 0,
         title: "Click for more info",
-        link: "https://www.thesitewizard.com/html-tutorial/open-links-in-new-window-or-tab.shtml"
+        link: "https://www.macalester.edu/isp/governmentregulations/employmentpostmac/#opt"
       },
       {
         id: 1,
-        title: "Download form",
-        link: "https://www.thesitewizard.com/html-tutorial/open-links-in-new-window-or-tab.shtml"
+        title: "Access the form",
+        link: "https://docs.google.com/forms/d/e/1FAIpQLScxOI4jI86El6_QKMqZqqdB7NeA0O8yWEd4C_vo0kDTiw-3RQ/viewform"
       }
     ]
   },
   {
     id: 2,
-    date: "1999-01-01",
+    date: "2020-04-07",
     checked: false,
-    title: "Some Title #3",
-    description: "Some description #3",
-    notes: "",
+    title: "Check/Money order",
+    description: "Check or money order for $410 made out to the Department of Homeland Security.",
+    notes: "Payment with a valid credit card is possible with Form G-1450 Authorization for Credit Card Transactions",
     subTasks: [
       {
         id: 0,
-        title: "Subtask #1",
+        title: "Learn how to fill out a check/money order",
         checked: false
       }
     ],
@@ -71,26 +71,31 @@ export const MOCK_TASKS = [
       {
         id: 0,
         title: "Click for more info",
-        link: "https://www.thesitewizard.com/html-tutorial/open-links-in-new-window-or-tab.shtml"
+        link: "https://www.macalester.edu/isp/governmentregulations/employmentpostmac/#opt"
       },
       {
         id: 1,
-        title: "Download form",
-        link: "https://www.thesitewizard.com/html-tutorial/open-links-in-new-window-or-tab.shtml"
+        title: "How to write a check",
+        link: "https://www.macalester.edu/isp/wp-content/uploads/sites/95/2017/02/HowToWriteACheck.pdf"
+      },
+      {
+        id: 2,
+        title: "How to pay with credit card",
+        link: "https://www.uscis.gov/forms/fingerprints/pay-a-credit-card"
       }
     ]
   },
   {
     id: 3,
-    date: "1999-01-01",
+    date: "2020-04-10",
     checked: false,
-    title: "Some Title #3",
-    description: "Some description #3",
-    notes: "",
+    title: "Form G-1145",
+    description: "Filling out this form is optional but recommended, and you will receive a text message or email when USCIS receives your application.",
+    notes: "Use this form to request a text message or email when USCIS receives your application",
     subTasks: [
       {
         id: 0,
-        title: "Subtask #1",
+        title: "Download the form from the USCIS website",
         checked: false
       }
     ],
@@ -98,26 +103,36 @@ export const MOCK_TASKS = [
       {
         id: 0,
         title: "Click for more info",
-        link: "https://www.thesitewizard.com/html-tutorial/open-links-in-new-window-or-tab.shtml"
+        link: "https://www.macalester.edu/isp/governmentregulations/employmentpostmac/#opt"
       },
       {
         id: 1,
         title: "Download form",
-        link: "https://www.thesitewizard.com/html-tutorial/open-links-in-new-window-or-tab.shtml"
+        link: "https://www.uscis.gov/system/files_force/files/form/g-1145.pdf?download=1"
       }
     ]
   },
   {
     id: 4,
-    date: "1999-01-01",
+    date: "2020-04-15",
     checked: false,
-    title: "Some Title #3",
-    description: "Some description #3",
+    title: "Form I-765",
+    description: "Certain aliens who are in the United States may file Form I-765, Application for Employment Authorization, to request employment authorization and an Employment Authorization Document (EAD).  Other aliens whose immigration status authorizes them to work in the United States without restrictions may also use Form I-765 to apply to U.S. Citizenship and Immigration Services (USCIS) for an EAD that shows such authorization.",
     notes: "",
     subTasks: [
       {
         id: 0,
-        title: "Subtask #1",
+        title: "For Line 1.a., check 'Initial permission to accept employment.",
+        checked: false
+      },
+      {
+        id: 1,
+        title: "For Lines 5.a. - 7.d., If you authorize ISP to open your mail, make sure to put “c/o ISP, 1600 Grand Ave, St Paul, MN 55105” as your mailing address on the Form I-765 and put your actual address as your physical address.",
+        checked: false
+      },
+      {
+        id: 2,
+        title: "For Line 27 Eligibility Category, enter code (c) (3) (B) for post-completion or graduation OPT; (c) (3) (C) is for STEM OPT extension, and (c) (3) (A) is for pre-completion.",
         checked: false
       }
     ],
@@ -125,26 +140,26 @@ export const MOCK_TASKS = [
       {
         id: 0,
         title: "Click for more info",
-        link: "https://www.thesitewizard.com/html-tutorial/open-links-in-new-window-or-tab.shtml"
+        link: "https://www.macalester.edu/isp/governmentregulations/employmentpostmac/#opt"
       },
       {
         id: 1,
         title: "Download form",
-        link: "https://www.thesitewizard.com/html-tutorial/open-links-in-new-window-or-tab.shtml"
+        link: "https://www.uscis.gov/system/files_force/files/form/i-765.pdf?download=1"
       }
     ]
   },
   {
     id: 5,
-    date: "1999-01-01",
+    date: "2020-04-20",
     checked: false,
-    title: "Some Title #3",
-    description: "Some description #3",
-    notes: "",
+    title: "Copy of F-1 visa page",
+    description: "Copy of F-1 visa page typically found in your passport.",
+    notes: "Should be clear and easy to read",
     subTasks: [
       {
         id: 0,
-        title: "Subtask #1",
+        title: "Make a copy of F-1 page",
         checked: false
       }
     ],
@@ -152,26 +167,21 @@ export const MOCK_TASKS = [
       {
         id: 0,
         title: "Click for more info",
-        link: "https://www.thesitewizard.com/html-tutorial/open-links-in-new-window-or-tab.shtml"
-      },
-      {
-        id: 1,
-        title: "Download form",
-        link: "https://www.thesitewizard.com/html-tutorial/open-links-in-new-window-or-tab.shtml"
+        link: "https://www.macalester.edu/isp/governmentregulations/employmentpostmac/#opt"
       }
     ]
   },
   {
     id: 6,
-    date: "1999-01-01",
+    date: "2020-04-25",
     checked: false,
-    title: "Some Title #3",
-    description: "Some description #3",
+    title: "Copy of most recent I-94",
+    description: "You may access the form I-94 on the DHS website",
     notes: "",
     subTasks: [
       {
         id: 0,
-        title: "Subtask #1",
+        title: "Enter the necessary information and download your most recent form I-94",
         checked: false
       }
     ],
@@ -184,21 +194,21 @@ export const MOCK_TASKS = [
       {
         id: 1,
         title: "Download form",
-        link: "https://www.thesitewizard.com/html-tutorial/open-links-in-new-window-or-tab.shtml"
+        link: "https://i94.cbp.dhs.gov/I94/#/home"
       }
     ]
   },
   {
     id: 7,
-    date: "1999-01-01",
+    date: "2020-04-30",
     checked: false,
-    title: "Some Title #3",
-    description: "Some description #3",
+    title: "[ISP] CPT history",
+    description: "ISP will print your CPT history if applicable",
     notes: "",
     subTasks: [
       {
         id: 0,
-        title: "Subtask #1",
+        title: "Email ISP at isp@macalester.edu to ask them to print your CPT history if applicable",
         checked: false
       }
     ],
@@ -206,39 +216,38 @@ export const MOCK_TASKS = [
       {
         id: 0,
         title: "Click for more info",
-        link: "https://www.thesitewizard.com/html-tutorial/open-links-in-new-window-or-tab.shtml"
-      },
-      {
-        id: 1,
-        title: "Download form",
-        link: "https://www.thesitewizard.com/html-tutorial/open-links-in-new-window-or-tab.shtml"
+        link: "https://www.macalester.edu/isp/governmentregulations/employmentpostmac/#opt"
       }
     ]
   },
   {
     id: 8,
-    date: "1999-01-01",
+    date: "2020-05-05",
     checked: false,
-    title: "Some Title #3",
-    description: "Some description #3",
-    notes: "",
+    title: "I-20",
+    description: "ISP will print new I-20 for you to submit with your Form I-765",
+    notes: "Complete OPT application must be received by USCIS within 30 days of new I-20 issuance, failure to do so will result in application’s rejection",
     subTasks: [
       {
         id: 0,
-        title: "Subtask #1",
+        title: "Sign your I-20",
         checked: false
+      },
+      {
+        id: 1,
+        title:"Submit complete OPT application within 30 days of I-20 issuance"
       }
     ],
     actionItems: [
       {
         id: 0,
         title: "Click for more info",
-        link: "https://www.thesitewizard.com/html-tutorial/open-links-in-new-window-or-tab.shtml"
+        link: "https://www.macalester.edu/isp/governmentregulations/employmentpostmac/#opt"
       },
       {
         id: 1,
-        title: "Download form",
-        link: "https://www.thesitewizard.com/html-tutorial/open-links-in-new-window-or-tab.shtml"
+        title: "Check status of OPT application",
+        link: "https://egov.uscis.gov/casestatus/landing.do"
       }
     ]
   }
