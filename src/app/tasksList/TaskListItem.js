42ec78682853b3b0df25f997d4bc5099253c892b
Kevin Shin <kevinshin@Kevins-MacBook-Pro.local>
2019-12-06 10:17:06 -0600

    Implements stylistic changes to TaskListItem.

--- src/app/tasksList/TaskListItem.js
+++ src/app/tasksList/TaskListItem.js
@@ -1,4 +1,4 @@
-import {updateTask} from "../../common/api";
+import { updateTask } from "../../common/api";
 import React from "react";
 
 const TaskListItem = (props) => {
@@ -9,43 +9,45 @@ const TaskListItem = (props) => {
       className="TaskListItem"
       onClick={() => props.onSelect(task)}
     >
-      <div>
-        <input
-          type="date"
-          value={task.date}
-          onChange={(e) => {
-            e.persist();
+      <div className="task-item">
+        <div>
+          <input
+            type="date"
+            value={task.date}
+            onChange={(e) => {
+              e.persist();
 
-            let newTask = {
-              ...task,
-              date: e.target.value
-            };
+              let newTask = {
+                ...task,
+                date: e.target.value
+              };
 
-            updateTask(task.id, newTask);
-            props.onChange(newTask);
-          }}
-        />
-        <span className="task-li-title">
-          {props.task.title}
-        </span>
-      </div>
-      <div>
-        <input
-          type="checkbox"
-          checked={task.checked}
-          onChange={(e) => {
-            e.persist();
-
-            let newTask = {
-              ...task,
-              checked: e.target.checked
-            };
-
-            updateTask(task.id, newTask);
-            props.onChange(newTask);
-          }}
-        />
+              updateTask(task.id, newTask);
+              props.onChange(newTask);
+            }}
+          />
+          <span className="task-li-title">
+            {props.task.title}
+          </span>
         </div>
+        <div>
+          <input
+            type="checkbox"
+            checked={task.checked}
+            onChange={(e) => {
+              e.persist();
+
+              let newTask = {
+                ...task,
+                checked: e.target.checked
+              };
+
+              updateTask(task.id, newTask);
+              props.onChange(newTask);
+            }}
+          />
+        </div>
+      </div>
     </li>
   )
 };
